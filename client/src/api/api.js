ceac570876cd6a218201617406381a4c58bd1548
Necro-sword <gokulkumarn2k4@gmail.com>
2025-05-27 20:40:28 +0530

    deploy test

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://job-management-admin-interface-olive.vercel.app/api';
+const API_BASE_URL = 'https://job-management-interface-flax.vercel.app/api';
 
 // const API_BASE_URL = 'http://localhost:5000/api'; 
 
